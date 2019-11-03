ecfe02d3a1a9e5da4aaf1f2b94e237c81d66c18e
TheAeonianDuke <abhishek18211@iiitd.ac.in>
2019-11-03 11:48:11 +0530

    Added Ingame Menu

--- src/MainGame/LawnController.java
+++ src/MainGame/LawnController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -20,9 +21,19 @@ import java.util.ResourceBundle;
 
 public class LawnController implements Initializable
 {
+    TranslateTransition movelawnmower;
+    TranslateTransition movezombie;
+    TranslateTransition movesun;
+    TranslateTransition movepea;
 
     @FXML
     private Pane Lawn;
+
+    @FXML
+    private Button exit_to_menu;
+
+    @FXML
+    private Button return_game;
 
     @FXML
     private Pane menu_panel;
@@ -42,30 +53,30 @@ public class LawnController implements Initializable
     @FXML
     private void shootPea()
     {
-        TranslateTransition move = new TranslateTransition(Duration.seconds(2),Pea);
-        move.setToX(Pea.getLayoutX() + 1000);
-        move.setCycleCount(10);
-        move.play();
+        movepea = new TranslateTransition(Duration.seconds(2),Pea);
+        movepea.setToX(Pea.getLayoutX() + 1000);
+        movepea.setCycleCount(10);
+        movepea.play();
     }
 
     @FXML
     private void FallSun()
     {
-        TranslateTransition move = new TranslateTransition(Duration.seconds(6),FallSun);
-        move.setToY(FallSun.getLayoutY() + 760);
+        movesun = new TranslateTransition(Duration.seconds(6),FallSun);
+        movesun.setToY(FallSun.getLayoutY() + 760);
 //        move.setCycleCount(10);
-        move.play();
+        movesun.play();
     }
 
     private void moveZombie()
     {
 
-        TranslateTransition move = new TranslateTransition(Duration.seconds(8),Zombie);
-        move.setToX(Zombie.getLayoutX() - 2280);
+        movezombie = new TranslateTransition(Duration.seconds(8),Zombie);
+        movezombie.setToX(Zombie.getLayoutX() - 2280);
 //        System.out.println(Zombie.getLayoutX()+" "+ LawnMower0.getLayoutX());
-        move.setCycleCount(1);
-        move.play();
-        move.setOnFinished(new EventHandler<ActionEvent>(){
+        movezombie.setCycleCount(1);
+        movezombie.play();
+        movezombie.setOnFinished(new EventHandler<ActionEvent>(){
 
             @Override
             public void handle(ActionEvent arg0) {
@@ -79,17 +90,22 @@ public class LawnController implements Initializable
 
     private void triggerLawnMower()
     {
-        TranslateTransition move = new TranslateTransition(Duration.seconds(3),LawnMower0);
-        move.setDelay(Duration.millis(7900));
-        move.setToX(LawnMower0.getLayoutX() + 1000);
-        move.setCycleCount(1);
-        move.play();
+        movelawnmower = new TranslateTransition(Duration.seconds(3),LawnMower0);
+        movelawnmower.setDelay(Duration.millis(7900));
+        movelawnmower.setToX(LawnMower0.getLayoutX() + 1000);
+        movelawnmower.setCycleCount(1);
+        movelawnmower.play();
     }
 
     @FXML
     private void MenuBtn(javafx.event.ActionEvent actionEvent) throws IOException
     {
         menu_panel.setVisible(true);
+        movelawnmower.pause();
+        movezombie.pause();
+        movesun.pause();
+        movepea.pause();
+
     }
 
 
@@ -103,4 +119,17 @@ public class LawnController implements Initializable
     }
 
 
+    public void ExitToMenu(ActionEvent actionEvent) throws IOException {
+        Pane pane = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+        Lawn.getChildren().setAll(pane);
+    }
+
+    public void ReturnGame(ActionEvent actionEvent)
+    {
+        menu_panel.setVisible(false);
+        movelawnmower.play();
+        movezombie.play();
+        movesun.play();
+        movepea.play();
+    }
 }
